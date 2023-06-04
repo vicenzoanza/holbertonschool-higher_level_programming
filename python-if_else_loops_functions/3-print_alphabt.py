d49badca5854655bc855c32fa6d137aa5c02a785
vicenzoanza <viceanza2010@hotmail.com>
2023-06-04 14:46:57 -0700

    task 3

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(97,123):
-    if chr(i) == 'e' or chr(i) == 'q':
-        continue
-    print("{}".format(chr(i)), end="")
+for char in range(ord("a"), ord("z") + 1):
+    if chr(char) not in "qe":
+        print("{}".format(chr(char)), end="")
+

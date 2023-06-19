988ebf99d023f024bbecbf9bcb6f18a0590f4111
vicenzoanza <viceanza2010@hotmail.com>
2023-06-18 19:21:25 -0700

    task 2

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -38,5 +38,8 @@ class Rectangle:
 
     def perimeter(self):
         """ calculate perimeter rectangle """
-        return (self.__width * 2) + (self.__height * 2)
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return (self.__width * 2) + (self.__height * 2)
 

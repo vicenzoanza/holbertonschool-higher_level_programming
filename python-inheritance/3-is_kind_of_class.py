751076f37ea57de3950682b6978bbac080f90f72
vicenzoanza <viceanza2010@hotmail.com>
2023-06-24 10:01:08 -0700

    task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+""" task 3 """
+def is_kind_of_class(obj, a_class):
+    """ returns True if the object is an instance of, or if the object is an instance of a class """
+    return isinstance(obj, a_class)

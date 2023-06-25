2bf2e96ddc4b011fecb27d40c5fbce181869c9a7
vicenzoanza <viceanza2010@hotmail.com>
2023-06-24 20:06:49 -0700

    task 4

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import json
+""" task 4 """
+def from_json_string(my_str):
+    """ returns an object (Python data structure) represented by a JSON string """
+    return json.loads(my_str)

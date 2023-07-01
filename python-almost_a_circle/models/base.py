3c999565aa50a8f75ebafcbffd2651e482296ebd
vicenzoanza <viceanza2010@hotmail.com>
2023-07-01 11:46:10 -0700

    task 2

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """ task 1 """
 class Base:
-""" class base
+    """
+    class base
 
-__nb_objects = Is the counter of objects
+    __nb_objects = Is the counter of objects
 
 
-"""
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """ Initialize
+        """
+        Initialize
         id = identifier
         """
         if id is not None:

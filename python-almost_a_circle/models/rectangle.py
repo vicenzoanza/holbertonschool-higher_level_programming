26addfe4405dd7fb6c41182a11d4820e629a347f
vicenzoanza <viceanza2010@hotmail.com>
2023-07-01 11:52:34 -0700

    task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """ task 2 """
+
 from models.base import Base
+
 class Rectangle(Base):
+    """
+    class Rectangle that inherits from Base
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        width = width of rectangle
+        height = height of rectangle
+        x = position x of rectangle
+        y = position y of rectangle
+        id = identifier of rectangle
+        """
         super().__init__(id)
         self.width = width
         self.height = height

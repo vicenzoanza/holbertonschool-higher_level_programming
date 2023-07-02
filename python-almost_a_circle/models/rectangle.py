5bb293c1a09fd4d88cfcb3ac44a3225cddba9a44
vicenzoanza <viceanza2010@hotmail.com>
2023-07-02 15:09:16 -0700

    task 8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -96,3 +96,18 @@ class Rectangle(Base):
             print("")
         for n in range (self.height):
             print(" " * self.x + "#" * self.width)
+
+    def update(self, *args):
+        """
+        assigns an argument to each attribute
+        """
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]

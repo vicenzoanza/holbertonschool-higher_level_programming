00b6a93eff487c8069b9202e383ad20089692276
vicenzoanza <viceanza2010@hotmail.com>
2023-07-02 15:37:21 -0700

    task 10

---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" square """
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Initialice
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """
+        return [Square] (<id>) <x>/<y> - <size> - in our case, width or height
+        """
+        return(f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}")
+

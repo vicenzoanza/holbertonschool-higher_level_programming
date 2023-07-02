d5c0a2d626a804cd96feedaa1294f33e08115941
vicenzoanza <viceanza2010@hotmail.com>
2023-07-02 15:39:03 -0700

    task 10

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -3,7 +3,9 @@
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
-
+    """
+    class Square that inherits from Rectangle
+    """
     def __init__(self, size, x=0, y=0, id=None):
         """
         Initialice

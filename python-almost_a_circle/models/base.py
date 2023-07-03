d0bb13ec2e606f6f2cab237b2593010d1baf3ade
vicenzoanza <viceanza2010@hotmail.com>
2023-07-02 17:25:08 -0700

    task 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,3 +33,12 @@ class Base:
             return "[]"
         else:
             return json.dumps(list_dictionaries)
+
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string
+        """
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)

ea6f6772e85a6d21c60423f2fe4af747f62b2502
vicenzoanza <viceanza2010@hotmail.com>
2023-07-27 06:24:13 -0300

    task 5

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,3 +1,2 @@
 -- script that prints the full description of the table "first_table" from the database "hbtn_0c_0" in your MySQL server
-DESC first_table;
--- Also can be "DESCRIBE"
+SHOW TABLE first_table;

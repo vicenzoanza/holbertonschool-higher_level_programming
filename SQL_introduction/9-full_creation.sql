4367edc8ef19854d11c40bb2927d1f08e1663a40
vicenzoanza <viceanza2010@hotmail.com>
2023-07-29 10:27:44 -0300

    task 9

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,11 @@
+-- script that creates a table "second_table" in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+CREATE TABLE IF NOT EXISTS second_table (
+    id int NOT NULL AUTO-INCREMENT,
+    name varchar(256),
+    score int,
+    PRIMARY KEY (id)
+);
+INSERT INTO second_table (name, score) VALUES ('John', 10);
+INSERT INTO second_table (name, score) VALUES ('Alex', 3);
+INSERT INTO second_table (name, score) VALUES ('Bob', 14);
+INSERT INTO second_table (name), score VALUES ('George', 8);

1d227f1b14e4f444d2c32defa49f2d87492f6375
vicenzoanza <viceanza2010@hotmail.com>
2023-08-04 08:22:12 -0700

    task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -13,4 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             print("out of range")
             result.append(0)
+        finally:
+            continue
+
     return result
